d6390499216aa76f3bfca730eacf9d7cbf1771da
yuanyuyuan <az6980522@gmail.com>
2023-12-28 18:47:34 +0800

    cargo fmt

--- io/zenoh-transport/src/unicast/transport_unicast_inner.rs
+++ io/zenoh-transport/src/unicast/transport_unicast_inner.rs
@@ -16,9 +16,9 @@ use crate::{
     unicast::{link::TransportLinkUnicast, TransportConfigUnicast},
     TransportPeerEventHandler,
 };
-use tokio::sync::MutexGuard as AsyncMutexGuard;
 use async_trait::async_trait;
 use std::{fmt::DebugStruct, sync::Arc, time::Duration};
+use tokio::sync::MutexGuard as AsyncMutexGuard;
 use zenoh_link::Link;
 use zenoh_protocol::{
     core::{WhatAmI, ZenohId},
