e8f4212bbc6b4641df4ec4bc34b27924b6e740c4
yuanyuyuan <az6980522@gmail.com>
2024-01-04 17:24:06 +0800

    Tokio porting on udp

--- io/zenoh-links/zenoh-link-udp/src/unicast.rs
+++ io/zenoh-links/zenoh-link-udp/src/unicast.rs
@@ -15,18 +15,16 @@ use super::{
     get_udp_addrs, socket_addr_to_udp_locator, UDP_ACCEPT_THROTTLE_TIME, UDP_DEFAULT_MTU,
     UDP_MAX_MTU,
 };
-use async_std::net::{Ipv4Addr, Ipv6Addr, SocketAddr, UdpSocket};
-use async_std::prelude::*;
-use async_std::sync::Mutex as AsyncMutex;
-use async_std::task;
-use async_std::task::JoinHandle;
 use async_trait::async_trait;
 use std::collections::HashMap;
 use std::fmt;
 use std::net::IpAddr;
-use std::sync::atomic::{AtomicBool, Ordering};
+use std::net::{Ipv4Addr, Ipv6Addr, SocketAddr};
 use std::sync::{Arc, Mutex, RwLock, Weak};
 use std::time::Duration;
+use tokio::net::UdpSocket;
+use tokio::sync::Mutex as AsyncMutex;
+use tokio_util::{sync::CancellationToken, task::TaskTracker};
 use zenoh_core::{zasynclock, zlock, zread, zwrite};
 use zenoh_link_commons::{
     ConstructibleLinkManagerUnicast, LinkManagerUnicastTrait, LinkUnicast, LinkUnicastTrait,
@@ -35,7 +33,6 @@ use zenoh_link_commons::{
 use zenoh_protocol::core::{EndPoint, Locator};
 use zenoh_result::{bail, zerror, Error as ZError, ZResult};
 use zenoh_sync::Mvar;
-use zenoh_sync::Signal;
 
 type LinkHashMap = Arc<Mutex<HashMap<(SocketAddr, SocketAddr), Weak<LinkUnicastUdpUnconnected>>>>;
 type LinkInput = (Vec<u8>, usize);
@@ -240,24 +237,27 @@ impl fmt::Debug for LinkUnicastUdp {
 /*************************************/
 struct ListenerUnicastUdp {
     endpoint: EndPoint,
-    active: Arc<AtomicBool>,
-    signal: Signal,
-    handle: JoinHandle<ZResult<()>>,
+    token: CancellationToken,
+    tracker: TaskTracker,
 }
 
 impl ListenerUnicastUdp {
     fn new(
         endpoint: EndPoint,
-        active: Arc<AtomicBool>,
-        signal: Signal,
-        handle: JoinHandle<ZResult<()>>,
+        token: CancellationToken,
+        tracker: TaskTracker,
     ) -> ListenerUnicastUdp {
         ListenerUnicastUdp {
             endpoint,
-            active,
-            signal,
-            handle,
+            token,
+            tracker,
         }
+    }
+
+    async fn stop(&self) {
+        self.token.cancel();
+        self.tracker.close();
+        self.tracker.wait().await;
     }
 }
 
@@ -399,24 +399,25 @@ impl LinkManagerUnicastTrait for LinkManagerUnicastUdp {
                     )?;
 
                     // Spawn the accept loop for the listener
-                    let active = Arc::new(AtomicBool::new(true));
-                    let signal = Signal::new();
+                    let token = CancellationToken::new();
+                    let c_token = token.clone();
                     let mut listeners = zwrite!(self.listeners);
 
-                    let c_active = active.clone();
-                    let c_signal = signal.clone();
                     let c_manager = self.manager.clone();
                     let c_listeners = self.listeners.clone();
                     let c_addr = local_addr;
-                    let handle = task::spawn(async move {
+
+                    let tracker = TaskTracker::new();
+                    let task = async move {
                         // Wait for the accept loop to terminate
-                        let res = accept_read_task(socket, c_active, c_signal, c_manager).await;
+                        let res = accept_read_task(socket, c_token, c_manager).await;
                         zwrite!(c_listeners).remove(&c_addr);
                         res
-                    });
+                    };
+                    tracker.spawn_on(task, &zenoh_runtime::ZRuntime::TX);
 
                     let locator = endpoint.to_locator();
-                    let listener = ListenerUnicastUdp::new(endpoint, active, signal, handle);
+                    let listener = ListenerUnicastUdp::new(endpoint, token, tracker);
                     // Update the list of active listeners on the manager
                     listeners.insert(local_addr, listener);
 
@@ -464,9 +465,8 @@ impl LinkManagerUnicastTrait for LinkManagerUnicastUdp {
         match listener {
             Some(l) => {
                 // Send the stop signal
-                l.active.store(false, Ordering::Release);
-                l.signal.trigger();
-                l.handle.await
+                l.stop().await;
+                Ok(())
             }
             None => {
                 bail!(
@@ -518,8 +518,7 @@ impl LinkManagerUnicastTrait for LinkManagerUnicastUdp {
 
 async fn accept_read_task(
     socket: UdpSocket,
-    active: Arc<AtomicBool>,
-    signal: Signal,
+    token: CancellationToken,
     manager: NewLinkChannelSender,
 ) -> ZResult<()> {
     let socket = Arc::new(socket);
@@ -543,19 +542,9 @@ async fn accept_read_task(
         };
     }
 
-    enum Action {
-        Receive((usize, SocketAddr)),
-        Stop,
-    }
-
-    async fn receive(socket: Arc<UdpSocket>, buffer: &mut [u8]) -> ZResult<Action> {
+    async fn receive(socket: Arc<UdpSocket>, buffer: &mut [u8]) -> ZResult<(usize, SocketAddr)> {
         let res = socket.recv_from(buffer).await.map_err(|e| zerror!(e))?;
-        Ok(Action::Receive(res))
-    }
-
-    async fn stop(signal: Signal) -> ZResult<Action> {
-        signal.wait().await;
-        Ok(Action::Stop)
+        Ok(res)
     }
 
     let src_addr = socket.local_addr().map_err(|e| {
@@ -565,65 +554,67 @@ async fn accept_read_task(
     })?;
 
     log::trace!("Ready to accept UDP connections on: {:?}", src_addr);
-    // Buffers for deserialization
-    while active.load(Ordering::Acquire) {
-        let mut buff = zenoh_buffers::vec::uninit(UDP_MAX_MTU as usize);
-        // Wait for incoming connections
-        let (n, dst_addr) = match receive(socket.clone(), &mut buff)
-            .race(stop(signal.clone()))
-            .await
-        {
-            Ok(action) => match action {
-                Action::Receive((n, addr)) => (n, addr),
-                Action::Stop => break,
-            },
-            Err(e) => {
-                log::warn!("{}. Hint: increase the system open file limit.", e);
-                // Throttle the accept loop upon an error
-                // NOTE: This might be due to various factors. However, the most common case is that
-                //       the process has reached the maximum number of open files in the system. On
-                //       Linux systems this limit can be changed by using the "ulimit" command line
-                //       tool. In case of systemd-based systems, this can be changed by using the
-                //       "sysctl" command line tool.
-                task::sleep(Duration::from_micros(*UDP_ACCEPT_THROTTLE_TIME)).await;
-                continue;
-            }
-        };
 
-        let link = loop {
-            let res = zgetlink!(src_addr, dst_addr);
-            match res {
-                Some(link) => break link.upgrade(),
-                None => {
-                    // A new peers has sent data to this socket
-                    log::debug!("Accepted UDP connection on {}: {}", src_addr, dst_addr);
-                    let unconnected = Arc::new(LinkUnicastUdpUnconnected {
-                        socket: Arc::downgrade(&socket),
-                        links: links.clone(),
-                        input: Mvar::new(),
-                        leftover: AsyncMutex::new(None),
-                    });
-                    zaddlink!(src_addr, dst_addr, Arc::downgrade(&unconnected));
-                    // Create the new link object
-                    let link = Arc::new(LinkUnicastUdp::new(
-                        src_addr,
-                        dst_addr,
-                        LinkUnicastUdpVariant::Unconnected(unconnected),
-                    ));
-                    // Add the new link to the set of connected peers
-                    if let Err(e) = manager.send_async(LinkUnicast(link)).await {
-                        log::error!("{}-{}: {}", file!(), line!(), e)
+    loop {
+        // Buffers for deserialization
+        let mut buff = zenoh_buffers::vec::uninit(UDP_MAX_MTU as usize);
+
+        tokio::select! {
+            _ = token.cancelled() => break,
+
+            res = receive(socket.clone(), &mut buff) => {
+                match res {
+                    Ok((n, dst_addr)) => {
+                        let link = loop {
+                            let res = zgetlink!(src_addr, dst_addr);
+                            match res {
+                                Some(link) => break link.upgrade(),
+                                None => {
+                                    // A new peers has sent data to this socket
+                                    log::debug!("Accepted UDP connection on {}: {}", src_addr, dst_addr);
+                                    let unconnected = Arc::new(LinkUnicastUdpUnconnected {
+                                        socket: Arc::downgrade(&socket),
+                                        links: links.clone(),
+                                        input: Mvar::new(),
+                                        leftover: AsyncMutex::new(None),
+                                    });
+                                    zaddlink!(src_addr, dst_addr, Arc::downgrade(&unconnected));
+                                    // Create the new link object
+                                    let link = Arc::new(LinkUnicastUdp::new(
+                                        src_addr,
+                                        dst_addr,
+                                        LinkUnicastUdpVariant::Unconnected(unconnected),
+                                    ));
+                                    // Add the new link to the set of connected peers
+                                    if let Err(e) = manager.send_async(LinkUnicast(link)).await {
+                                        log::error!("{}-{}: {}", file!(), line!(), e)
+                                    }
+                                }
+                            }
+                        };
+
+                        match link {
+                            Some(link) => {
+                                link.received(buff, n).await;
+                            }
+                            None => {
+                                zdellink!(src_addr, dst_addr);
+                            }
+                        }
+                    }
+
+                    Err(e) => {
+                        log::warn!("{}. Hint: increase the system open file limit.", e);
+                        // Throttle the accept loop upon an error
+                        // NOTE: This might be due to various factors. However, the most common case is that
+                        //       the process has reached the maximum number of open files in the system. On
+                        //       Linux systems this limit can be changed by using the "ulimit" command line
+                        //       tool. In case of systemd-based systems, this can be changed by using the
+                        //       "sysctl" command line tool.
+                        tokio::time::sleep(Duration::from_micros(*UDP_ACCEPT_THROTTLE_TIME)).await;
+                        continue;
                     }
                 }
-            }
-        };
-
-        match link {
-            Some(link) => {
-                link.received(buff, n).await;
-            }
-            None => {
-                zdellink!(src_addr, dst_addr);
             }
         }
     }
